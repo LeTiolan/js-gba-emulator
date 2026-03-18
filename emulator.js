f399adec727d745d3ace410b9f793fe667677433
LeTiolan <knockoffelonmusk@gmail.com>
2026-03-18 01:09:20 -0700

    Update emulator.js

--- emulator.js
+++ emulator.js
@@ -816,7 +816,7 @@ const CoreBridge = {
            let safeCode = code.replace(/import\.meta\.url/g, 'window.location.href');
                 
                // THE FIX: We fixed the import bug, so we can let the Workers live!
-            const moduleSetup = "var Module = { 'noExitRuntime': true, 'arguments': [], 'locateFile': function(p) { return 'https://letiolan.github.io/Quartz-GBA/' + p; }, 'mainScriptUrlOrBlob': window.coreBlobUrl };\n";
+          const moduleSetup = "var Module = { 'noExitRuntime': true, 'arguments': [], 'locateFile': function(p) { if(p.endsWith('.wasm')) return 'https://letiolan.github.io/Quartz-GBA/core.wasm'; return 'https://letiolan.github.io/Quartz-GBA/' + p; }, 'mainScriptUrlOrBlob': window.coreBlobUrl };\n";
                safeCode = moduleSetup + safeCode;
                 
                 safeCode = safeCode.replace(/export\s+default.*/g, '');
@@ -940,10 +940,11 @@ const CoreBridge = {
         
         console.log("[System] Attempting to ignite mGBA WASM Core...");
 
-      window.mGBA({
+     window.mGBA({
             canvas: document.getElementById('screen'),
             mainScriptUrlOrBlob: window.coreBlobUrl, 
             locateFile: function(path) {
+                if (path.endsWith('.wasm')) return 'https://letiolan.github.io/Quartz-GBA/core.wasm';
                 return 'https://letiolan.github.io/Quartz-GBA/' + path;
             }
         }).then(function(Module) {
